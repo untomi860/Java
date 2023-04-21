ab8a7eaeb78958597f66d1a4016512e9e8f7f839
Untomi860 <Untomi860@gmail.com>
2023-04-21 20:32:32 +0700

    dobavil_ewe

---
+++ Six lesson/Screen.java
@@ -0,0 +1,25 @@
+public class Screen
+    {
+        private String description;
+
+
+        public Screen(String description)
+        {
+            this.description = description;
+        }
+
+        public void up()
+        {
+            System.out.println(description + " going up");
+        }
+
+        public void down()
+        {
+            System.out.println(description + " going down");
+        }
+
+        public String ToString()
+        {
+            return description;
+        }
+}
--- Six lesson/Tuner.java
+++ Six lesson/Tuner.java
@@ -1,11 +1,36 @@
-private Amplifier;
 
+public class Tuner {
+    private String description;
+    private Amplifier amplifier;
+    private double frequency;
 
-public class Tuner{
-    Tuner.on();
-    Tuner.off();
-    Tuner.setAm();
-    Tuner.setFm();
-    Tuner.setFrequency();
-    Tuner.toString();
+    public Tuner(String description, Amplifier amplifier) {
+        this.description = description;
+        this.amplifier = amplifier;
+    }
+
+    public void on() {
+        System.out.println(description + " on");
+    }
+
+    public void off() {
+        System.out.println(description + " off");
+    }
+
+    public void setFrequency(double frequency) {
+        System.out.println(description + " setting frequency to " + frequency);
+        this.frequency = frequency;
+    }
+
+    public void setAm() {
+        System.out.println(description + " setting AM mode");
+    }
+
+    public void setFm() {
+        System.out.println(description + " setting FM mode");
+    }
+
+    public String toString() {
+        return description;
+    }
 }
